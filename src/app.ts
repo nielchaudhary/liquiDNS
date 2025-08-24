367fb7b0032c2b49bb0c83e5cd69f9607e675fef
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-08-25 00:28:37 +0530

    added scripts, config files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import { Logger } from "./config/logger";
 dotenv.config();
+
+const logger = new Logger("app-server");
 
 const PORT = process.env.PORT ?? 8090;
 
 app.listen(PORT, () => {
-  console.log("LiquiDNS SERVER LIVE ON PORT 8090");
+  logger.info("liquidns SERVER LIVE ON PORT 8090");
 });
