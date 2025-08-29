f4c5ec26b8e366830dc8ae6ba4a3a2dca5c2fef5
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-08-30 03:38:41 +0530

    added domain service

--- src/domain/api/get-domains.ts
+++ src/domain/api/get-domains.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
 import { getErrorDetails } from "../../config/error";
 import { Logger } from "../../config/logger";
+
 const logger = new Logger("get-domain");
+
+/* 
+https://domainr.com/docs/api - using domainr api to get available domains
+*/
+
 export const getAvailableDomains = (req: Request, res: Response) => {
   try {
   } catch (error) {
